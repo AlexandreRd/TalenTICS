fc95185bd96ff7c56c097a79ecb0f896c8863d9c
Alejandro Reséndiz Martínez <a01273960@HGOOSXCITLA02.local>
2016-12-13 18:20:49 -0600

    datos actualizados xDxdXd

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 // Importas librerias
 var express = require('express');
+var bodyParser = require('body-parser');
 // Creas Server
 var app = express();
 // Rutas
+app.use(bodyParser.urlencoded( {
+	extended:true
+}));
+
 app.get('/', (req, res) => {
-	res.send('O no, Jac?');
+	// res.send('<h1>O no, Jac?</h1>');
+	res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/weey', (req, res) => {
+	res.send("Huixcazdhaaaa!!!");
+});
+
+app.post('/mi_ruta', (req, res) => {
+	console.log(req.body);
+	res.send("OK xDxdXd");
 });
 
 app.listen(8000, () => {
